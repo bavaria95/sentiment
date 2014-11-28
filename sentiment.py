50987b3e1d1d174968884b9c8a899fcb2f358582
bavaria95 <bavaria95@gmail.com>
2014-11-28 22:17:08 +0100

    rewrote main algorithm

--- sentiment.py
+++ sentiment.py
@@ -1,71 +1,65 @@
 import pandas as pd
 import sys, os
-import math
-import nltk
+import math, nltk, re
  
 file_to_read = r"train.tsv"
-train_data_pandas = pd.read_table(file_to_read)
+train_data = pd.read_table(file_to_read)
  
 features = []
 
-size = train_data_pandas.count()[0]
+size = train_data.count()[0]
 
-train_data_pandas['Unigrams'] = train_data_pandas['Phrase'].str.split() 
+#? lower case phrases
+
+
+train_data['Unigrams'] = train_data['Phrase'].str.split() 
 
 bigrams = [] 												
 for i in range(size):
-    t = list(nltk.bigrams(train_data_pandas['Phrase'][i].split()))	
+    t = list(nltk.bigrams(train_data['Phrase'][i].split()))	
     t = list(map(lambda x: x[0] + ' ' + x[1], t))
     bigrams.append(t)
 
-train_data_pandas['Bigrams'] = bigrams
+train_data['Bigrams'] = bigrams
 
-print train_data_pandas
+# print train_data
 
- 
-# # ---------------------------------------------------------------V to ograniczenie pozniej nalezy usunac [H]
-for index,number,phraseid,phrase,sentiment in train_data_pandas[0:1].itertuples():
-# iterujemy poszczegolne linie z danych jako tuple, index = number -1, nie ma potrzeby przeksztalcania tabeli danych [H]
-    V = 0
-    d = {}
-    words = phrase.split()
-    print 'list = ', words
- 
-    for word in words:
-        d[word] = d.setdefault(word, 0) + 1
- 
-    for word in d:
-        C = d[word]
-        P = train_data_pandas[train_data_pandas['Sentiment'] == 0].count()[0]
-        N = train_data_pandas[train_data_pandas['Sentiment'] == 2].count()[0]
-        Pt = 0
-        Nt = 0   
-        #wybieramy tylko columny frazy i sentymentu, tworzoymy nowy 'dataframe' [H]
-        phrase_and_sentiment_columns = train_data_pandas[['Phrase','Sentiment']]
-        print phrase_and_sentiment_columns # odkomentuj sobie i zobacz jak teraz to wyglada [H]
-        #wybieramy tylko pierwszy wpis (mozna usunac, zalezy co chcesz zrobic) [H]
-        phrase_and_sentiment_columns = phrase_and_sentiment_columns[0:1]
-        # --V numer linii, V fraza, V wartosc saentymentu----------------V iterujemy po liniach jako tuple [H]
-        for col_id,phrase_in, sentiment_in in phrase_and_sentiment_columns.itertuples():
-            print ("diff info", phrase_in.split(), sentiment_in)
-            if word in phrase_in.split() and sentiment_in == 0:
-            	print ('THERE IS PHRASE to Pt!')
-                Pt += 1
-            elif word in phrase_in.split() and sentiment_in == 2:
-            	print ('THERE IS PHRASE to Nt!')
-                Nt += 1
- 
-        print 'for formula: ', word, C, P, N, Pt, Nt
- 
-        #dzielisz tutaj przez zero, dlatego jest blad [H]
-        V_l = C * math.log((N * Pt)/(P * Nt), 2) #nie wiem dlaczego nie rachuje poprawnie, chociaz jak policzyc to osobno z 
-        print V_l      #wyzej wypisanymi wynikami - ma byc dobrze...
-        V += V_l
- 
-        features.append(V)
- 
- 
-print(features)
+
+for index, number, phraseid, phrase, sentiment, unigrams, bigrams in train_data[0:117].itertuples():
+	V = 0
+
+	V_uni = 0
+	V_bi = 0
+
+	N_t = 0
+	P_t = 0
+
+	P = train_data[train_data['Sentiment'] == 0].count()[0]
+
+	N = train_data[train_data['Sentiment'] == 2].count()[0]
+
+
+	for uni in unigrams:
+		uni = re.escape(uni)
+
+		N_t_t = train_data[(train_data['Sentiment'] == 0) & (train_data['Phrase'].str.contains('\W' + uni + '\W'))].count()[0]
+		P_t_t = train_data[(train_data['Sentiment'] == 2) & (train_data['Phrase'].str.contains('\W' + uni + '\W'))].count()[0]
+
+		C_uni = len(re.findall('\W?' + uni + '\W?', phrase))
+
+
+		if P * N_t_t != 0 and N * P_t_t != 0:
+			V_uni = C_uni * math.log((N * P_t_t) / (P * N_t_t), 2)
+			print (uni, C_uni, P, N, P_t_t, N_t_t, V_uni)
+		else:
+			print (uni, C_uni, P, N, P_t_t, N_t_t)			
+
+
+	# for bi in bigrams:
+	# 	N_t_bi += train_data[(train_data['Sentiment'] == 0) & (train_data['Phrase'].str.contains(bi))].count()[0]
+
+
+
 # jak nie wiemy co to bedzie pozytywny i negatywny to moim zdaniem bedzie sensownie zgenerowac takie features()
 		# 0 2
 		# 0 3
